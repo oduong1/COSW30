01776fffd4e4fc84113a0686c8dab9eda81e39c2
EC2 Default User <ec2-user@ip-172-31-89-165.ec2.internal>
2019-09-02 19:07:20 +0000

    My first commit

---
+++ labwork/lab1/lab1.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html>
+    
+    <head>
+        <meta charset="utf-8">
+
+        <title>Labwork 1: Hello World</title>
+
+    </head>
+    <body>
+        
+        
+        <h1>Oanh Duong</h1>
+        <h2>This is first PHP page about Bacon Ipsum!</h2>
+        
+        
+        <p>
+            Kielbasa pork chop shankle, turkey tail pig beef ball tip. Meatball shankle pork belly pastrami sirloin. 
+            Pork chop porchetta cupim, ground round cow bacon jerky tri-tip boudin chicken. 
+            Picanha ground round venison, tongue beef meatloaf short ribs jowl pig shoulder drumstick meatball pastrami. 
+            Kevin ham hock andouille picanha, prosciutto tail flank boudin beef short ribs brisket pork loin pork belly ball tip hamburger. 
+            Chuck venison ham hock bacon tail shoulder.
+        </p>
+        
+        <a href="https://github.com/oduong1/COSW30">This is my github profile</a>
+
+        <?php
+            print'<p><a href=""> This is my PHP paragraph </a></p>';
+            print '<p>This is the link to <a href="https://www.codecademy.com/">codecademy website, 
+            </a>where one can learn many things from web development to computer programming, for free.</p>';
+            print'<p><a href="https://www.youtube.com/">YouTube</a> is another source to learn all cool things</p>';
+            print'<p>This assignment is really tough for me, as I had troubles with accessing my aws account in class.</p>';
+            print'<p>Because of this, I got behind and pushed my work onto a wrong account.</p>'; 
+            print'<p>I am now deleting my Cloud9 environment and start fresh. I really hope it works.</p>';
+            print'<p>Well, if you see this message, then I am in.</p>';
+        ?>
+ </body>
+ </html>
