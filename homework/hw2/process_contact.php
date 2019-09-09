1da97e94a4ec38b6b413f6ffdc3a5fd6ca3af86d
EC2 Default User <ec2-user@ip-172-31-89-165.ec2.internal>
2019-09-09 02:51:14 +0000

    update contact forms 2nd time

--- homework/hw2/process_contact.php
+++ homework/hw2/process_contact.php
@@ -8,22 +8,36 @@
 	body {
 		font-family: 'Gayathri', sans-serif;
 		color: #300ACC;
+		margin: 10px 25px;
+		text-align: left;
 	}
 
 	</style>
 </head>
 <body>
 <?php 
+
 // Print the submitted information:
    $age = $_POST['age'];
+   $gender = $_POST['gender'];
+   $amount = $_POST['amount'];
 	if ($age == "0-17") {
 		echo "<p>Thank you for your interest but you are too young.</p>";
 		echo "<p>Come back when you are over 18 years of age.</p>";
 	}
-   else {
-	echo "<h4>Thank you, <strong>{$_POST['name']}</strong>, for your interest in our campaign</h4>
-	<pre>{$_POST['comments']}</pre>
-	<p>We will send you an email at <em>{$_POST['email']}</em>.</p>\n";
+
+   else {	
+	echo "<h4>Hello, <strong>{$_POST['name']}</strong></h4>";
+	
+		if ($amount > 0) {
+			echo "<h5>Thank you for your contribution of <em>$amount</em>.</h5>";
+			echo "<p>We will send you an email confirmation at <em>{$_POST['email']}</em>.</p>\n";
+		}	
+		else {
+			echo "<h5>Thank you for your interest in our campaign.</h5>";
+			echo "<p>We will send you a follow-up email at <em>{$_POST['email']}</em>.</p>\n";
+		}
+
    }
 ?>
 </body>
